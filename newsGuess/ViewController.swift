ea43314f582228b02fa627de32fe238b3a2b1f85
rokirovochka <nmaksakovskii@gmail.com>
2019-11-25 23:40:33 +0700

    Initial Commit

---
+++ newsGuess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsGuess
+//
+//  Created by Никита Максаковский on 25.11.2019.
+//  Copyright © 2019 Никита Максаковский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
